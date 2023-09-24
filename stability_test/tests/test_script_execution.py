d50a21103f46098d7012d0cec9d9140f58481f20
Marcus Wieder <31651017+wiederm@users.noreply.github.com>
2023-09-25 00:34:28 +0200

    update location of scripts

--- stability_test/tests/test_script_execution.py
+++ stability_test/tests/test_script_execution.py
@@ -1,7 +1,61 @@
 import subprocess
 
 
-def test_stability_tests_script(
+def generate_args(script: str):
+    args = {
+        "hipen_vacuum_args": [  # Check if script can be executed with the correct arguments for a HiPen molecule
+            "python",
+            script,
+            "vacuum",
+            "1",
+            "ani2x",
+            "nnpops",
+            "50",
+        ],
+        "waterbox_args": [  # Check if script can be executed with the correct arguments for a waterbox
+            "python",
+            script,
+            "waterbox",
+            "20",
+            "NpT",
+            "ani2x",
+            "torchani",
+            "False",
+            "10",
+        ],
+        "alanine_dipeptide_solvent_args": [  # Check if script can be executed with the correct arguments for a alanine dipeptide in solvent
+            "python",
+            script,
+            "alanine-dipeptide",
+            "solvent",
+            "ani2x",
+            "torchani",
+            "NpT",
+            "10",
+        ],
+        "alanine_dipeptide_vacuum_args": [  # Check if script can be executed with the correct arguments for a alanine dipeptide in vacuum
+            "python",
+            script,
+            "alanine-dipeptide",
+            "vacuum",
+            "ani2x",
+            "torchani",
+            "",
+            "10",
+        ],
+        "DOF_ethane_args": [  # Check if script can be executed with the correct arguments for a alanine dipeptide in vacuum        "python",
+            script,
+            "DOF",
+            "ani2x",
+            "torchani",
+            "{'bond' : [0, 2]}",
+            "ethanol",
+        ],
+    }
+    return args
+
+
+def test_script_execution_vacuum(
     script: str = "scripts/perform_stability_tests.py",
 ) -> None:
     print(f"Testing {script}")
@@ -10,73 +64,60 @@ def test_stability_tests_script(
     print(ret.stdout.decode("utf-8"))
     assert ret.returncode == 0
 
-    # Check if script can be executed with the correct arguments for a HiPen molecule
-    hipen_vacuum_args = [
-        "python",
-        script,
-        "vacuum",
-        "1",
-        "ani2x",
-        "nnpops",
-        "50",
-    ]
-    # Check if script can be executed with the correct arguments for a waterbox
+    args = generate_args(script)["hipen_vacuum_args"]
 
-    waterbox_args = [
-        "python",
-        script,
-        "waterbox",
-        "20",
-        "NpT",
-        "ani2x",
-        "torchani",
-        "False",
-        "10",
-    ]
-    # Check if script can be executed with the correct arguments for a alanine dipeptide in solvent
-    alanine_dipeptide_solvent_args = [
-        "python",
-        script,
-        "alanine-dipeptide",
-        "solvent",
-        "ani2x",
-        "torchani",
-        "NpT",
-        "10",
-    ]
-    # Check if script can be executed with the correct arguments for a alanine dipeptide in vacuum
-    alanine_dipeptide_vacuum_args = [
-        "python",
-        script,
-        "alanine-dipeptide",
-        "vacuum",
-        "ani2x",
-        "torchani",
-        "",
-        "10",
-    ]
-    # Check if script can be executed with the correct arguments for a alanine dipeptide in vacuum
-    DOF_ethane_args = [
-        "python",
-        script,
-        "DOF",
-        "ani2x",
-        "torchani",
-        "{'bond' : [0, 2]}",
-        "ethanol",
-    ]
+    ret = subprocess.run(
+        args,
+        capture_output=True,
+    )
 
-    for args in [
-        hipen_vacuum_args,
-        # waterbox_args,
-        # alanine_dipeptide_solvent_args,
-        alanine_dipeptide_vacuum_args,
-        DOF_ethane_args,
-    ]:
-        print(ret.stdout.decode("utf-8"))
-        assert ret.returncode == 0
 
-        ret = subprocess.run(
-            args,
-            capture_output=True,
-        )
+def test_script_execution_waterbox(
+    script: str = "scripts/perform_stability_tests.py",
+) -> None:
+    print(f"Testing {script}")
+    # Check if script exists and can be executed
+    ret = subprocess.run(["python", script, "--help"], capture_output=True)
+    print(ret.stdout.decode("utf-8"))
+    assert ret.returncode == 0
+
+    args = generate_args(script)["waterbox_args"]
+
+    ret = subprocess.run(
+        args,
+        capture_output=True,
+    )
+
+
+def test_script_execution_dipeptide_solvent(
+    script: str = "scripts/perform_stability_tests.py",
+) -> None:
+    print(f"Testing {script}")
+    # Check if script exists and can be executed
+    ret = subprocess.run(["python", script, "--help"], capture_output=True)
+    print(ret.stdout.decode("utf-8"))
+    assert ret.returncode == 0
+
+    args = generate_args(script)["alanine_dipeptide_solvent_args"]
+
+    ret = subprocess.run(
+        args,
+        capture_output=True,
+    )
+
+
+def test_script_execution_dipeptide_vacuum(
+    script: str = "scripts/perform_stability_tests.py",
+) -> None:
+    print(f"Testing {script}")
+    # Check if script exists and can be executed
+    ret = subprocess.run(["python", script, "--help"], capture_output=True)
+    print(ret.stdout.decode("utf-8"))
+    assert ret.returncode == 0
+
+    args = generate_args(script)["alanine_dipeptide_vacuum_args"]
+
+    ret = subprocess.run(
+        args,
+        capture_output=True,
+    )
