e02c38aff93c41a4c2716bcbe071e13d4cc0120f
wiederm <marcus.wieder@univie.ac.at>
2023-09-26 02:11:13 +0200

    update

--- stability_test/analysis.py
+++ stability_test/analysis.py
@@ -7,6 +7,12 @@ class PropertyCalculator:
         self.md_traj = md_traj
 
     def calculate_water_rdf(self):  # type: ignore
+        """
+        Calculates the radial distribution function (RDF) for water molecules in the trajectory.
+
+        Returns:
+        - mdtraj_rdf (np.ndarray): The RDF values for the water molecules.
+        """
         # Expression selection, a common feature of analysis tools for
         oxygen_pairs = self.md_traj.top.select_pairs(
             "name O and water", "name O and water"
@@ -22,6 +28,12 @@ class PropertyCalculator:
         return mdtraj_rdf
 
     def monitor_water_bond_length(self):  # type: ignore
+        """
+        Monitors the bond length between water molecules in the trajectory.
+
+        Returns:
+        - bond_length (np.ndarray): The bond lengths between water molecules.
+        """
         bond_list = []
         for bond in self.md_traj.topology.bonds:
             if bond.atom1.residue.name == "HOH" and bond.atom2.residue.name == "HOH":
@@ -30,6 +42,12 @@ class PropertyCalculator:
         return self.monitor_bond_length(bond_list)
 
     def monitor_water_angle(self):  # type: ignore
+        """
+        Monitors the angle between water molecules in the trajectory.
+
+        Returns:
+        - angles (np.ndarray): The angles between water molecules.
+        """
         def _extract_angles() -> list:
             angle_list = []
             for bond_1 in self.md_traj.top.bonds:
@@ -58,9 +76,27 @@ class PropertyCalculator:
         return self.monitor_angle_length(angle_list)
 
     def monitor_bond_length(self, bond_pairs: list):  # type: ignore
+        """
+        Monitors the bond length between a given set of atoms in the trajectory.
+
+        Args:
+        - bond_pairs (list): A list of tuples representing the pairs of atoms to monitor.
+
+        Returns:
+        - bond_length (np.ndarray): The bond lengths between the specified atoms.
+        """
         bond_length = md.compute_distances(self.md_traj, bond_pairs)
         return bond_length
 
     def monitor_angle_length(self, angle_list: list):  # type: ignore
+        """
+        Monitors the angle between a given set of atoms in the trajectory.
+
+        Args:
+        - angle_list (list): A list of tuples representing the sets of atoms to monitor.
+
+        Returns:
+        - angles (np.ndarray): The angles between the specified atoms.
+        """
         angles = md.compute_angles(self.md_traj, angle_list) * (180 / np.pi)
         return angles
--- stability_test/protocolls.py
+++ stability_test/protocolls.py
@@ -19,16 +19,55 @@ log = logging.getLogger("stability")
 
 # StateDataReporter with custom print function
 class ContinuousProgressReporter(object):
+    """
+    A class for reporting the progress of a simulation in a continuous manner.
+    """
+
     def __init__(self, iostream: TextIO, total_steps: int, reportInterval: int):
+        """
+        Initializes a ContinuousProgressReporter object.
+
+        Parameters
+        ----------
+        iostream : TextIO
+            The output stream to write the progress report to.
+        total_steps : int
+            The total number of steps in the simulation.
+        reportInterval : int
+            The interval at which to report the progress.
+        """
         self._out = iostream
         self._reportInterval = reportInterval
         self._total_steps = total_steps
 
     def describeNextReport(self, simulation: Simulation) -> Tuple:
+        """
+        Returns information about the next report.
+
+        Parameters
+        ----------
+        simulation : Simulation
+            The simulation to report on.
+
+        Returns
+        -------
+        Tuple
+            A tuple containing information about the next report.
+        """
         steps = self._reportInterval - simulation.currentStep % self._reportInterval
         return (steps, False, False, True, False, None)
 
     def report(self, simulation: Simulation, state: State) -> None:
+        """
+        Reports the progress of the simulation.
+
+        Parameters
+        ----------
+        simulation : Simulation
+            The simulation to report on.
+        state : State
+            The state of the simulation.
+        """
         progress = 100.0 * simulation.currentStep / self._total_steps
         self._out.write(f"\rProgress: {progress:.2f}")
         self._out.flush()
@@ -36,6 +75,10 @@ class ContinuousProgressReporter(object):
 
 @dataclass
 class StabilityTestParameters:
+    """
+    A dataclass for storing parameters for a stability test.
+    """
+
     protocol_length: int
     temperature: unit.Quantity
     ensemble: str
@@ -50,6 +93,10 @@ class StabilityTestParameters:
 
 @dataclass
 class DOFTestParameters:
+    """
+    A dataclass for storing parameters for a degree of freedom (DOF) test.
+    """
+
     system: System
     platform: Platform
     testsystem: TestSystem
@@ -61,12 +108,35 @@ class DOFTestParameters:
 
 
 class DOFTest(ABC):
+    """
+    An abstract base class for degree of freedom (DOF) tests.
+    """
+
     def __init__(self) -> None:
         self.potential_simulation_factory = SimulationFactory()
 
     def _set_bond_length(
         self, position: unit.Quantity, atom1: int, atom2: int, length: float
     ) -> unit.Quantity:
+        """
+        Sets the length of a bond between two atoms.
+
+        Parameters
+        ----------
+        position : unit.Quantity
+            The positions of the atoms.
+        atom1 : int
+            The index of the first atom.
+        atom2 : int
+            The index of the second atom.
+        length : float
+            The desired length of the bond.
+
+        Returns
+        -------
+        unit.Quantity
+            The new positions of the atoms.
+        """
         diff = (position[atom2] - position[atom1]).value_in_unit(unit.angstrom)
         normalized_diff = diff / np.linalg.norm(diff)
         new_positions = position.copy()
@@ -82,6 +152,187 @@ class DOFTest(ABC):
         qsim: Simulation,
         parameters: DOFTestParameters,
     ) -> Tuple[List, List, List]:
+        """
+        Performs a scan of the length of a bond between two atoms.
+
+        Parameters
+        ----------
+        qsim : Simulation
+            The simulation to perform the scan on.
+        parameters : DOFTestParameters
+            The parameters for the DOF test.
+
+        Returns
+        -------
+        Tuple[List, List, List]
+            A tuple containing the conformations, potential energies, and bond lengths.
+        """
+        assert parameters.bond is not None
+        bond_atom1, bond_atom2 = parameters.bond
+        initial_pos = parameters.testsystem.positions
+        conformations, potential_energy, bond_length = [], [], []
+        for b in np.linspace(0, 8, 100):  # in angstrom
+            new_pos = self._set_bond_length(
+                initial_pos,
+                bond_atom1,
+                bond_atom2,
+                b,
+            )
+            qsim.context.setPositions(new_pos)
+            energy = qsim.context.getState(getEnergy=True).getPotentialEnergy()
+            potential_energy.append(energy.value_in_unit(unit.kilojoule_per_mole))
+            conformations.append(new_pos.value_in_unit(unit.nano * unit.meter))
+            bond_length.append(b)
+
+    def describeNextReport(self, simulation: Simulation) -> Tuple:
+        steps = self._reportInterval - simulation.currentStep % self._reportInterval
+        return (steps, False, False, True, False, None)
+
+    def report(self, simulation: Simulation, state: State) -> None:
+        progress = 100.0 * simulation.currentStep / self._total_steps
+        self._out.write(f"\rProgress: {progress:.2f}")
+        self._out.flush()
+
+
+@dataclass
+class StabilityTestParameters:
+    """
+    A dataclass for storing parameters for a stability test.
+
+    Attributes
+    ----------
+    protocol_length : int
+        The length of the protocol to run.
+    temperature : unit.Quantity
+        The temperature to run the simulation at.
+    ensemble : str
+        The ensemble to simulate.
+    simulated_annealing : bool
+        Whether to use simulated annealing.
+    system : System
+        The system to simulate.
+    platform : Platform
+        The platform to run the simulation on.
+    testsystem : TestSystem
+        The test system to simulate.
+    output_folder : str
+        The path to the output folder.
+    log_file_name : str
+        The name of the log file.
+    state_data_reporter : StateDataReporter
+        The reporter to use for state data.
+    """
+
+    protocol_length: int
+    temperature: unit.Quantity
+    ensemble: str
+    simulated_annealing: bool
+    system: System
+    platform: Platform
+    testsystem: TestSystem
+    output_folder: str
+    log_file_name: str
+    state_data_reporter: StateDataReporter
+
+
+@dataclass
+class DOFTestParameters:
+    """
+    A dataclass for storing parameters for a degree of freedom (DOF) test.
+
+    Attributes
+    ----------
+    protocol_length : int
+        The length of the protocol to run.
+    temperature : unit.Quantity
+        The temperature to run the simulation at.
+    ensemble : str
+        The ensemble to simulate.
+    simulated_annealing : bool
+        Whether to use simulated annealing.
+    system : System
+        The system to simulate.
+    platform : Platform
+        The platform to run the simulation on.
+    testsystem : TestSystem
+        The test system to simulate.
+    output_folder : str
+        The path to the output folder.
+    log_file_name : str
+        The name of the log file.
+    state_data_reporter : StateDataReporter
+        The reporter to use for state data.
+    """
+
+    system: System
+    platform: Platform
+    testsystem: TestSystem
+    output_folder: str
+    log_file_name: str
+    bond: List = field(default_factory=lambda: [])
+    angle: List = field(default_factory=lambda: [])
+    torsion: List = field(default_factory=lambda: [])
+
+
+class DOFTest(ABC):
+    def __init__(self) -> None:
+        """
+        Initializes a new instance of the StabilityTest class.
+        """
+        self.potential_simulation_factory = SimulationFactory()
+
+    def _set_bond_length(
+        self, position: unit.Quantity, atom1: int, atom2: int, length: float
+    ) -> unit.Quantity:
+        """
+        Sets the bond length between two atoms in a given position.
+
+        Parameters
+        ----------
+        position : unit.Quantity
+            The initial position of the atoms.
+        atom1 : int
+            The index of the first atom.
+        atom2 : int
+            The index of the second atom.
+        length : float
+            The desired bond length.
+
+        Returns
+        -------
+        unit.Quantity
+            The new position of the atoms with the updated bond length.
+        """
+        diff = (position[atom2] - position[atom1]).value_in_unit(unit.angstrom)
+        normalized_diff = diff / np.linalg.norm(diff)
+        new_positions = position.copy()
+        new_positions[atom2] = position[atom1] + normalized_diff * unit.Quantity(
+            length, unit.angstrom
+        )
+        log.debug(f"{position=}")
+        log.debug(f"{new_positions=}")
+        return unit.Quantity(new_positions, unit.angstrom)
+
+    def _perform_bond_scan(
+        self,
+        qsim: Simulation,
+        parameters: DOFTestParameters,
+    ) -> Tuple[List, List, List]:
+        """
+        Performs a scan of the bond length between two atoms in a given position.
+
+        Parameters
+        ----------
+        qsim : Simulation
+            The simulation object.
+        parameters : DOFTestParameters
+            The parameters for the degree of freedom test.
+
+        Returns
+        -------
+        Tuple[List, List, List]
+            A tuple containing the potential energy, conformations, and bond length.
+        """
         assert parameters.bond is not None
         bond_atom1, bond_atom2 = parameters.bond
         initial_pos = parameters.testsystem.positions
@@ -103,7 +354,25 @@ class DOFTest(ABC):
 
 
 class StabilityTest(ABC):
+    """
+    Abstract base class for stability tests on molecular systems.
+    """
+
     def __init__(self) -> None:
+        """
+        Initializes a BondProfileProtocol object.
+
+        This method initializes a BondProfileProtocol object and sets the potential_simulation_factory attribute to a new SimulationFactory object.
+        It also sets the implemented_ensembles attribute to a list of strings representing the ensembles that are implemented in the protocol.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
         self.potential_simulation_factory = SimulationFactory()
         self.implemented_ensembles = ["NpT", "NVT", "NVE"]
 
@@ -116,6 +385,22 @@ class StabilityTest(ABC):
         parameters: StabilityTestParameters,
         temperature: unit.Quantity,
     ) -> None:
+        """
+        Runs a simulation for stability tests on molecular systems.
+
+        This method runs a simulation for stability tests on molecular systems. It takes in a StabilityTestParameters object and a temperature as input parameters. It checks if the simulated annealing flag is set to True or False and if the ensemble is implemented in the protocol. It then creates a simulation object using the SimulationFactory class and sets the positions of the system. It minimizes the energy of the system and runs a simulated annealing molecular dynamics simulation if the simulated annealing flag is set to True. Finally, it writes out the simulation data to files.
+
+        Parameters
+        ----------
+        parameters: StabilityTestParameters
+            The parameters for the stability test.
+        temperature: unit.Quantity
+            The temperature of the simulation.
+
+        Returns
+        -------
+        None
+        """
         assert parameters.simulated_annealing in [True, False]
         if parameters.ensemble:
             assert parameters.ensemble in self.implemented_ensembles
@@ -205,12 +490,26 @@ class StabilityTest(ABC):
     ) -> None:
         pass
 
-
 class BondProfileProtocol(DOFTest):
     def __init__(self) -> None:
+        """
+        Initializes the StabilityTest class by calling the constructor of its parent class.
+        """
         super().__init__()
 
     def perform_bond_scan(self, parameters: DOFTestParameters) -> None:
+        """
+        Performs a bond scan on the given system and test system using the given parameters.
+
+        Parameters:
+        -----------
+        parameters : DOFTestParameters
+            The parameters to use for the bond scan.
+
+        Returns:
+        --------
+        None
+        """
         qsim = SimulationFactory.create_simulation(
             parameters.system,
             parameters.testsystem.topology,
@@ -240,23 +539,51 @@ class BondProfileProtocol(DOFTest):
 
 
 class MultiTemperatureProtocol(StabilityTest):
-    def __init__(self, ensemble: Union[None, str] = None) -> None:
-        super().__init__()
-        self.temperature_protcol = [
-            unit.Quantity(300, unit.kelvin),
-            unit.Quantity(600, unit.kelvin),
-            unit.Quantity(1_200, unit.kelvin),
-        ]
+    class PropagationProtocol(StabilityTest):
+        """
+        A class representing a propagation protocol for stability tests on molecular systems.
+        """
 
-        if ensemble:
-            assert ensemble in self.implemented_ensembles
-        self.ensemble = ensemble
+        def __init__(self, ensemble: Union[None, str] = None) -> None:
+            """
+            Initializes the PropagationProtocol class by calling the constructor of its parent class and setting the temperature protocol.
+
+            Parameters:
+            -----------
+            ensemble: Union[None, str], optional
+                The ensemble to use for the simulation. If None, the default ensemble is used.
+
+            Returns:
+            --------
+            None
+            """
+            super().__init__()
+            self.temperature_protcol = [
+                unit.Quantity(300, unit.kelvin),
+                unit.Quantity(600, unit.kelvin),
+                unit.Quantity(1_200, unit.kelvin),
+            ]
+
+            if ensemble:
+                assert ensemble in self.implemented_ensembles
+            self.ensemble = ensemble
 
     def perform_stability_test(
         self, StabilityTestParameters: StabilityTestParameters
     ) -> None:
+        """
+        Performs a stability test on the molecular system for each temperature in the temperature protocol by running a simulation.
+
+        Parameters:
+        -----------
+        StabilityTestParameters: StabilityTestParameters
+            The parameters for the stability test.
+
+        Returns:
+        --------
+        None
+        """
         for temperature in self.temperature_protcol:
-            # with mp.Pool(1) as pool:
             self._run_simulation(
                 StabilityTestParameters,
                 temperature,
@@ -265,6 +592,18 @@ class MultiTemperatureProtocol(StabilityTest):
 
 class PropagationProtocol(StabilityTest):
     def __init__(self, ensemble: Union[None, str] = None) -> None:
+        """
+        Initializes the PropagationProtocol class by calling the constructor of its parent class and setting the ensemble.
+
+        Parameters:
+        -----------
+        ensemble: Union[None, str], optional
+            The ensemble to use for the simulation. If None, the default ensemble is used.
+
+        Returns:
+        --------
+        None
+        """
         super().__init__()
         if ensemble:
             assert ensemble in self.implemented_ensembles
