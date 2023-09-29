dde5544b62aa06693305a15fc610c7171b4126ac
wiederm <marcus.wieder@univie.ac.at>
2023-09-29 18:36:35 +0200

    bugfix

--- guardowl/tests/test_system.py
+++ guardowl/tests/test_system.py
@@ -1,17 +1,16 @@
 from guardowl.setup import create_system_from_mol, generate_molecule
 from guardowl.testsystems import hipen_systems
-from .conftest import generate_hipen_system
 
 
-def test_generate_molecule() -> None:
+def test_generate_molecule(generate_hipen_system) -> None:
     """Test if we can generate a molecule instance"""
-    system, top, mol = generate_hipen_system()
+    system, top, mol = generate_hipen_system
     assert mol.n_conformers >= 1
 
 
-def test_generate_system_top_instances() -> None:
+def test_generate_system_top_instances(generate_hipen_system) -> None:
     """Test if we can generate a system/topology instance"""
-    system, topology, mol = generate_hipen_system()
+    system, topology, mol = generate_hipen_system
     topology = topology.to_openmm()
 
     indices = [atom.index for atom in topology.atoms()]
