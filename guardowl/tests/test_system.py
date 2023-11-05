358c9d837a16bc739bff1e3efc2c9d6b9bf4559f
wiederm <marcus.wieder@univie.ac.at>
2023-11-05 22:37:44 +0100

    update name

--- guardowl/tests/test_system.py
+++ guardowl/tests/test_system.py
@@ -1,4 +1,4 @@
-from guardowl.setup import create_system_from_mol, generate_molecule
+from guardowl.setup import create_system_from_mol, generate_molecule_from_smiles
 import pytest
 from guardowl.testsystems import PureLiquidTestsystemFactory, PureLiquidBoxTestSystem
 
@@ -21,11 +21,11 @@ def test_generate_system_top_instances(generate_hipen_system) -> None:
 @pytest.mark.parametrize(
     "molecule_name", PureLiquidTestsystemFactory._AVAILABLE_SYSTEM.keys()
 )
-@pytest.mark.parametrize(
-    "nr_of_copies",[100,200]
-)
+@pytest.mark.parametrize("nr_of_copies", [100, 200])
 def test_generate_pure_liquids(molecule_name, nr_of_copies) -> None:
     """ "Test if we can generate a pure liquid"""
 
     factory = PureLiquidTestsystemFactory()
-    factory.generate_testsystems(name=molecule_name, nr_of_copies=nr_of_copies, nr_of_equilibration_steps=1)
+    factory.generate_testsystems(
+        name=molecule_name, nr_of_copies=nr_of_copies, nr_of_equilibration_steps=1
+    )
